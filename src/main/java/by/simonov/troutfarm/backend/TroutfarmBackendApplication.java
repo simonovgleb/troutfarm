158e2164130cc258669d54a652658306b0cad077
Simonov Gleb <glebgithub@gmail.com>
2025-06-01 00:15:35 +0300

    init: add initial application structure.

---
+++ src/main/java/by/simonov/troutfarm/backend/TroutfarmBackendApplication.java
@@ -0,0 +1,13 @@
+package by.simonov.troutfarm.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroutfarmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroutfarmBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/simonov/troutfarm/backend/TroutfarmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package by.simonov.troutfarm.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroutfarmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
