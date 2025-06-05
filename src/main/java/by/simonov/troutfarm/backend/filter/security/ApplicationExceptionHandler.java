ebc43c80607b7059671522f4bf216c9f0cd62aea
Simonov Gleb <glebgithub@gmail.com>
2025-06-05 15:16:03 +0300

    feat: add Open API docs with Swagger.

--- src/main/java/by/simonov/troutfarm/backend/filter/security/ApplicationExceptionHandler.java
+++ src/main/java/by/simonov/troutfarm/backend/filter/security/ApplicationExceptionHandler.java
@@ -1,20 +1,23 @@
 package by.simonov.troutfarm.backend.filter.security;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
+@Hidden
+@ControllerAdvice(basePackages = {"by.simonov.troutfarm.backend.controller"})
 @Slf4j
 public class ApplicationExceptionHandler {
 
-    @ExceptionHandler(AuthorizationDeniedException.class)
-    public ResponseEntity<ProblemDetail> handleAuthorizationDenied(AuthorizationDeniedException ex) {
+    @ExceptionHandler({AuthorizationDeniedException.class, AuthenticationException.class})
+    public ResponseEntity<ProblemDetail> handleAuthorizationDenied(Exception ex) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ProblemDetail.forStatusAndDetail(
                 HttpStatus.UNAUTHORIZED,
                 ex.getMessage()
